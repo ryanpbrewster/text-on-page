f813895a3b08a35a554ab4ede6379d16eebc2457
Ryan P. Brewster <RyanPBrewster@gmail.com>
2017-12-19 09:39:04 -0800

    Force a download of the on-page text

--- on-page.js
+++ on-page.js
@@ -1,1 +1,14 @@
-console.log(document.body.innerText);
+function download(filename, text) {
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
+
+download("rpb.txt", document.body.innerText);
